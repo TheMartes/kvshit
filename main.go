0a5ce25f110b487a353671c9794fefc224bda389
Matej Buncek <matej.buncek@betsys.com>
2021-02-01 22:37:50 +0100

    rdy for grpc xD

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-
-	"./router"
-)
-
-func getHuh(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "huh\n")
-}
-
 func main() {
-	router.GetRoutes()
-
-	http.ListenAndServe(":8080", nil)
+	// :)
 }
